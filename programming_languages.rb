a789afa0eff81e44e8661b8954096cdd9ecb6771
Robert Peck <rspeckpfs@gmail.com>
2017-09-07 08:29:11 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each {|oo_or_functional, language_name_list|
+    language_name_list.each {|language_name, data|
+      data.each {|type_key, type_value|
+        if new_hash.include?(language_name)
+          new_hash[language_name][:style] << oo_or_functional #duplicate language names just need :oo/functional added to :style array. else, make a new one.
+        else
+        new_hash[language_name] = {type_key => type_value, :style => [oo_or_functional]} #using collect does not store the language_name symbol in the collected array. interesting.
+      end
+      }
+    }
+  }
+  new_hash
 end
